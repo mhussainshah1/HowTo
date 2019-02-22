583cc79864c683dcb6394840e4c453b161d1df08
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-22 01:54:06 -0500

    initial commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,48 +2,33 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/") //or @GetMapping("/index")
-    public String getIndex(){
+    public String getIndex() {
         return "index";
     }
 
     @GetMapping("/project")
-    public String getProject(){
+    public String getProject() {
         return "project";
     }
 
     @GetMapping("/compile")
-    public String getCompile(){
+    public String getCompile() {
         return "compile";
     }
 
     @GetMapping("/createjar")
-    public String getCreateJar(){
+    public String getCreateJar() {
         return "createjar";
     }
 
-
     @GetMapping("/run")
-    public String getRun(){
+    public String getRun() {
         return "run";
     }
 
-    @GetMapping("/signin")
-    public String getSignIn(){
-        return "signin";
-    }
 
-    @GetMapping("/form")
-    public String getForm(){
-        return "form";
-    }
-
-    @RequestMapping("/index2")
-    public String getIndex2(){
-        return "createjar";
-    }
 }
